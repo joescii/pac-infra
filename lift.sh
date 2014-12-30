94912c7a35fb48fae3ac7aacbea825d9bf20549e
Joe Barnes <barnesjd@gmail.com>
2014-12-30 17:50:39 -0600

    Trying another approach to building this crap

--- lift.sh
+++ lift.sh
@@ -3,16 +3,6 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 git clone https://github.com/lift/framework lift-framework
 cd lift-framework
-git checkout unified-parsers
-export SBT_OPTS="-Xms64M -Xmx256M -XX:MaxPermSize=512M"
-export SBT_CMD=publishLocal
-export SBT_JAR=${SBT_HOME}/bin/sbt-launch.jar
-
-${JAVA_HOME}/bin/java -ea -server ${JAVA_OPTS} ${SBT_OPTS}		\
-	-XX:+AggressiveOpts             		\
-	-XX:+OptimizeStringConcat			\
-	-XX:+UseConcMarkSweepGC               		\
-	-XX:+CMSParallelRemarkEnabled   		\
-	-XX:+CMSClassUnloadingEnabled   		\
-	-XX:+CMSIncrementalMode         		\
-	-jar project/${sbtjar} ${SBT_CMD}
+git checkout joe-snapshot
+chmod 700 ./sbt
+./sbt publishLocal
--- pac.sh
+++ pac.sh
@@ -3,5 +3,5 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 git clone https://github.com/joescii/prose-and-conz.git
 cd prose-and-conz
-export SBT_OPTS="-Xms64M -Xmx256M -XX:MaxPermSize=512M"
-sbt package
+chmod 700 ./sbt
+./sbt package
