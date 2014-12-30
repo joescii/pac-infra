c7a8f7a322b0ddbd449dc6d49d88c47fe7ab0234
Joe Barnes <barnesjd@gmail.com>
2014-12-30 16:20:51 -0600

    Trying another approach to auto-starting archiva

--- archiva.sh
+++ archiva.sh
@@ -3,10 +3,5 @@
 cd /usr/lib
 sudo wget http://mirror.cc.columbia.edu/pub/software/apache/archiva/1.3.9/binaries/apache-archiva-1.3.9.1-bin.zip
 sudo unzip apache-archiva-1.3.9.1-bin.zip
-
-sudo cat > /etc/init/archiva.conf <<- EOF
-script
-  /usr/lib/apache-archiva-1.3.9.1/bin/archiva start
-end script
-EOF
+sudo update-rc.d /usr/lib/apache-archiva-1.3.9.1/bin/archiva defaults 80
 
