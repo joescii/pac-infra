d43935385653f46841f23610d8f6c87e80fc75fc
Joe Barnes <barnesjd@gmail.com>
2014-12-30 14:23:05 -0600

    Actually adding archiva to the packer file

--- archiva.sh
+++ archiva.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
-wget http://mirrors.advancedhosters.com/apache/archiva/2.1.1/binaries/apache-archiva-2.1.1-bin.zip
-unzip apache-archiva-2.1.1-bin.zip
+cd /usr/lib
+sudo wget http://mirrors.advancedhosters.com/apache/archiva/2.1.1/binaries/apache-archiva-2.1.1-bin.zip
+sudo unzip apache-archiva-2.1.1-bin.zip
 cd apache-archiva-2.1.1/bin
-./archiva start
+sudo ./archiva start
