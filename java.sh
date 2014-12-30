c431f62b2d483176777fd2422704f6790cc97058
Joe Barnes <barnesjd@gmail.com>
2014-12-30 10:44:44 -0600

    Trying old name for software properties

--- java.sh
+++ java.sh
@@ -6,7 +6,7 @@
 # . ~/.bash_profile
 
 # Install Java 8
-sudo apt-get install -y software-properties-common
+sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
