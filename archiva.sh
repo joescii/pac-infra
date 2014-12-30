b2ce0510a9e3e2cfa7ddb105e8357f5dfde03969
Joe Barnes <barnesjd@gmail.com>
2014-12-30 16:55:35 -0600

    More auto-start archiva attempts

--- archiva.sh
+++ archiva.sh
@@ -3,6 +3,6 @@
 cd /usr/lib
 sudo wget http://mirror.cc.columbia.edu/pub/software/apache/archiva/1.3.9/binaries/apache-archiva-1.3.9.1-bin.zip
 sudo unzip apache-archiva-1.3.9.1-bin.zip
-sudo cp /usr/lib/apache-archiva-1.3.9.1/bin/archiva /etc/init.d/archiva
-sudo update-rc.d /usr/lib/apache-archiva-1.3.9.1/bin/archiva defaults 80
+sudo cp /usr/lib/apache-archiva-1.3.9/bin/archiva /etc/init.d/archiva
+sudo update-rc.d archiva defaults 80
 
