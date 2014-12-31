8fd776ddd03dfc69306cd202524e746e1f2b711a
Joe Barnes <barnesjd@gmail.com>
2014-12-31 06:33:40 -0600

    Adding some sudos

--- java7.sh
+++ java7.sh
@@ -5,8 +5,8 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get -y install oracle-java7-installer
-update-alternatives --display java
-apt-get -y install oracle-java7-set-default && apt-get clean
+sudo update-alternatives --display java
+sudo apt-get -y install oracle-java7-set-default && apt-get clean
 
