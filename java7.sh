0062b22422026e92e1f749a7323166f26491e314
Joe Barnes <barnesjd@gmail.com>
2014-12-31 06:39:36 -0600

    Removing a command that I don't know what it is needed for

--- java7.sh
+++ java7.sh
@@ -8,5 +8,5 @@ sudo apt-get update
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get -y install oracle-java7-installer
 sudo update-alternatives --display java
-sudo apt-get -y install oracle-java7-set-default && apt-get clean
+sudo apt-get -y install oracle-java7-set-default 
 
